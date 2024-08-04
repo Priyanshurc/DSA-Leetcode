e7c5be77b03335961d9ff8c7d9074affb700d8a3
Priyanshurc <104818892+Priyanshurc@users.noreply.github.com>
2024-08-04 22:06:08 +0530

    Add files via upload

---
+++ 42. Trapping Rain Water/Solution.java
@@ -0,0 +1,40 @@
+
+import java.util.*;
+import java.io.*;
+class Solution {
+
+    static int findWater(int [] arr, int n)
+    {
+        int left[] = new int[n];
+        int right[] = new int[n];
+
+        int water = 0;
+
+        left[0] = arr[0];
+        for (int i = 1; i < n; i++)
+            left[i] = Math.max(left[i - 1], arr[i]);
+
+        right[n - 1] = arr[n - 1];
+        for (int i = n - 2; i >= 0; i--)
+            right[i] = Math.max(right[i + 1], arr[i]);
+
+        for (int i = 0; i < n; i++)
+            water += Math.min(left[i], right[i]) - arr[i];
+
+        return water;
+    }
+
+    public static void main(String[] args){
+		Scanner inp = new Scanner(System.in);
+		int a = inp.nextInt();
+		int [] num = new int[a];
+
+		for (int i=0; i < a ; i++){
+			num[i] = inp.nextInt();
+		}
+		
+		Solution obj = new Solution();
+		int trapWater = obj.findWater(num,a);
+		System.out.println(trapWater);
+    }
+}
